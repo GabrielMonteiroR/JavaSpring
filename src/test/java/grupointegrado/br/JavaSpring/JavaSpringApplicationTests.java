2ba62ecdee9bf40b3d9f7de659f3cbfb63d5ecd8
Gabriel <gmonteiro810@gmail.com>
2024-11-06 19:30:40 -0300

    1-(initializing): initializing spring api

---
+++ src/test/java/grupointegrado/br/JavaSpring/JavaSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package grupointegrado.br.JavaSpring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
